16a8c64441ade9f8d5eeee9669a1adb39bf676ab
jawwad-ali <alijawwad001@gmail.com>
2020-10-20 14:51:24 +0500

    removed unused statements

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import styles from "./Footer.module.css"
-import FacebookIcon from '@material-ui/icons/Facebook';
-import GitHubIcon from '@material-ui/icons/GitHub';
 
 function Footer() {
     return (
